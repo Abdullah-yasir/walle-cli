db6fb53163d1e6d0546465d40f345d62c5dc5378
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-07-20 19:42:10 +0500

    new arguments support

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,37 @@
-#!/user/bin/env node
+#!/usr/bin/env node
+// const chalk = require("chalk");
+// const boxen = require("boxen");
 
-console.log('hello maky-cli');
+// const greeting = chalk.white.bold("Hello!");
+
+// const boxenOptions = {
+//   padding: 1,
+//   margin: 1,
+//   borderStyle: "round",
+//   borderColor: "green",
+//   backgroundColor: "#555555"
+// };
+// const msgBox = boxen(greeting, boxenOptions);
+
+// console.log(msgBox);
+
+
+const { boolean } = require("yargs");
+const yargs = require("yargs");
+
+const options = yargs
+  .option("c", { alias: "create", describe: "create", type: boolean, demandOption: true })
+  .option("s", { alias: "screen", describe: "create screen file", type: "string" })
+  .option("m", { alias: "module", describe: "create new component file", type: "string" })
+  .argv;
+
+console.log(options)
+if (options.c || options.create) {
+  if (options.s || options.screen) {
+    console.log('created screen ', options.s);
+  }
+  else if (options.m || options.module) {
+    console.log('created compoent ', options.m);
+  }
+}
+
