70282e16703cb421ef8d925090e9ac3745ecd3a7
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-07-22 15:04:43 +0500

    able to create new screen and folder as well if doesnt exist

--- bin/handlers/screen.js
+++ bin/handlers/screen.js
@@ -1,3 +1,46 @@
-module.exports = module.exports = (argv) => {
-  console.log(`setting ${argv.name}`)
+const fs = require('fs');
+const os = require('os');
+
+let screensFolder = './test/screens';
+
+const screenBody = (name) => {
+  return `
+  import React from 'react';
+  import { View, Text, StyleSheet } from 'react-native';
+
+  export const ${name} = (props) => {
+    const { children } = props;
+    return (
+      <View style={styles.screen}>
+        <Text>{children}</Text>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    screen:{}
+  });
+  `
+}
+
+module.exports = (argv) => {
+  // check if screens folder exists
+  // if doesn't try to create new one
+  try {
+    if (!fs.existsSync(screensFolder)) {
+      fs.mkdirSync(screensFolder)
+    }
+  } catch (err) {
+    console.log('Unable to create ./screens folder');
+  }
+  const screenName = argv.name;
+  const exportLine = `export { default as ${screenName} } from './${screenName}'; ${os.EOL}`
+  const fsCallback = (err) => {
+    if (err) console.log(err);
+    console.log(`created screen ${argv.name}`);
+  }
+  // create component file
+  fs.writeFile(`${screensFolder}/${screenName}.js`, screenBody(screenName), fsCallback);
+  // create import of comp in index file
+  fs.appendFile(`${screensFolder}/index.js`, exportLine, fsCallback)
 }
