20736122f009ba9d60d0e25daa118c5799666e37
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-07-22 14:33:58 +0500

    components are being created

--- bin/handlers/component.js
+++ bin/handlers/component.js
@@ -1,3 +1,38 @@
+const fs = require('fs');
+const os = require('os');
+
+const componentsFolder = './test/components';
+
+const compBody = (name) => {
+  return `
+  import React from 'react';
+  import {View, Text, StyleSheet} from 'react-native';
+
+  export const ${name} = (props) => {
+    const {children} = props;
+    return (
+      <View style={styles.component}>
+        <Text>{children}</Text>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    component:{}
+  });
+  `
+}
+
 module.exports = (argv) => {
-  console.log(`setting ${argv.name}`)
+  // capitalize first char of str
+  const componentName = argv.name;
+  const exportLine = `export { default as ${componentName} } from './${componentName}'; ${os.EOL}`
+  const fsCallback = (err) => {
+    if (err) console.log(err);
+    console.log(`created component ${argv.name}`);
+  }
+  // create component file
+  fs.writeFile(`${componentsFolder}/${componentName}.js`, compBody(componentName), fsCallback);
+  // create import of comp in index file
+  fs.appendFile(`${componentsFolder}/index.js`, exportLine, fsCallback)
 }
